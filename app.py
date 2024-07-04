48cbab85c198afc768636a60887244dff374a6e8
MarGlez10 <mcristy.gb@gmail.com>
2024-07-04 13:44:29 -0600

    agregamos la variable chart para el grafico de lines y la funcion toss_coin que emula el lanzamiento d euna moneda n veces y calcula la media en cada nueva iteracion, que se anade a chart

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
-import streamlit as st
+import scipy.stats
+import streamlit as st
+import time
 
 st.header('Lanzar una moneda')
+
+chart = st.line_chart([0.5])
+
+def toss_coin(n):
+
+    trial_outcomes = scipy.stats.bernoulli.rvs(p=0.5, size=n)
+
+    mean = None
+    outcome_no = 0
+    outcome_1_count = 0
+
+    for r in trial_outcomes:
+        outcome_no +=1
+        if r == 1:
+            outcome_1_count += 1
+        mean = outcome_1_count / outcome_no
+        chart.add_rows([mean])
+        time.sleep(0.05)
+
+    return mean
 
 number_of_trials = st.slider('¿Número de intentos?', 1, 1000, 10)
 start_button = st.button('Ejecutar')
 
 if start_button:
     st.write(f'Experimento con {number_of_trials} intentos en curso.')
-
-st.write('Esta aplicación aún no es funcional. En construcción.')
+    mean = toss_coin(number_of_trials)[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[B[A[A[A[A[A[A[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[A[A[A[A[A[A[A[A[A[A[A[D[D[D[D[D[D[D[D[D[D[i[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C
